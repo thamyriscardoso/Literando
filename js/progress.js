278d763af94e0082c8c1c32ce79854f11526ec83
Adrian Pastore <adrianqpastore@gmail.com>
2018-11-29 19:54:39 -0200

    Add files via upload

--- js/progress.js
+++ js/progress.js
@@ -1,26 +1,36 @@
 $(function() {
-    let $progress = $('#progress'),
-    $progressElements = $('.progress'),
-    TOTAL = $progressElements.length;
-    $progressElements.on('blur, change', function() {
-      let valid = $progressElements.filter(function() {
-        return ($(this).val() || $(this).prop('checked')) && !$(this).is(':invalid');
-      }).length;
-      let percent = (valid * 100) / TOTAL,
-          current = $progress.val();
+
+  var $progress         = $('#progress'), // Barra de Progresso.
+      $progressElements = $('.progress'), // Elementos que devem ser checados
+                                          // para modificar o valor da barra.
+      TOTAL             = $progressElements.length; // Total de elementos.
+
+  
+  $progressElements.on('blur, change', function() {
+    
+    // Faz um filtro com o total elementos válidos.
+    // Nesse caso, campos que não estejam "em branco" e que não estejam marcados
+    // como ':invalid'.
+    var valid = $progressElements.filter(function() {
+      return ($(this).val() || $(this).prop('checked')) && !$(this).is(':invalid');
+    }).length;
+    
+    // Calcula a porcentagem e altera o valor da barra.
+    var percent = (valid * 100) / TOTAL,
+        current = $progress.val();
+    
+    var increase = percent > current;
+        
+    var transition = setInterval(function(){
+      if((increase && current >= percent) ||
+        (!increase && current <= percent))
+          clearInterval(transition);
       
-      let increase = percent > current;
-          
-      let transition = setInterval(function(){
-        if((increase && current >= percent) ||
-          (!increase && current <= percent))
-            clearInterval(transition);
-        
-        let value = $progress.val();
-        value = increase ? value+1 : value-1;
-        current = value;
-        
-        $progress.val(current);
-      }, 10);    
-    });
+      var value = $progress.val();
+      value = increase ? value+1 : value-1;
+      current = value;
+      
+      $progress.val(current);
+    }, 10);    
   });
+});
