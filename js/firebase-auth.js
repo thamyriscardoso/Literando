c359e0aa9bf3deae14e67c867af29b62544776a6
thamyriscardoso <thamy_cardosoo@hotmail.com>
2018-12-01 19:15:43 -0200

    Update firebase-auth.js

--- js/firebase-auth.js
+++ js/firebase-auth.js
@@ -33,7 +33,6 @@ export const logout = function() {
         .then(function() {
             document.querySelector('#btnSair').style = 'display: none';
             document.querySelector('#logado').style = 'display: block';
-            window.location.replace('index.html');
             console.log('Deslogou');
         });
 };
