d770f37cc457279057c51025d2ca41e9a3b27d16
thamyriscardoso <thamy_cardosoo@hotmail.com>
2018-12-02 23:26:08 -0200

    Delete fullpagestyle.js

--- js/fullpagestyle.js
+++
@@ -1,6 +0,0 @@
-export const myFullpage = new fullpage ('#fullpage', {
-    sectionsColor: ['#1bbc9b', '#4BBFC3', '#7BAABE', '#ccddff', '#1bbc9b', '#4BBFC3', '#7BAABE', '#ccddff', '#1bbc9b', '#4BBFC3'],
-    anchors: ['firstPage', 'secondPage', '3rdPage', '4thpage', '5thpage', '6thpage', '7thpage', '8thpage', '9thpage', 'lastPage'],
-    menu: '#menu',
-    lazyLoad: true
-});
